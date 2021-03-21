0c697bccc03c885b537185fc785b5102856c7810
Amanda-G10 <reahana233@gmail.com>
2021-03-21 20:14:49 +0600

    data types

---
+++ chapter_two/datatype.js
@@ -0,0 +1,54 @@
+// primative
+
+// ***** Number *****
+
+var i =1234 // integer
+var f = 3.14 // float
+var nn = Number('45.4')
+var nn = Number(45.4)  // Number is an object here
+// console.log(nn); 
+// console.log(Number.parseFloat(nn));
+// console.log(Number.parseInt(nn));
+// console.log(Number.MAX_VALUE);
+// console.log(Number.MIN_VALUE);
+// console.log(1/0)  // Infinity
+// console.log('abc'* 2); // NaN
+
+//Hexadecimal
+
+// ***** String *****
+var str1 = 'String'
+var str2 = "String"
+var str3 =`String`
+var str4 = String('bgdua') //  String constractor here
+var str5 = String(123)
+var str6 = String(459.52)
+
+// console.log(str4, str5, str6 , str3);
+
+
+//***** Boolean *****
+var b1 = true
+var b2 = false
+
+var b3 = Boolean(true) 
+var b4 = Boolean(false)
+
+// console.log(b1,b2 ,b3, b4);
+
+
+// undefined // null
+// ***** null vs undefined *****
+var abc
+var xyz = null
+
+// console.log(abc)
+// console.log(xyz);
+
+
+// object
+// Array
+// Object
+// Function
+
+
