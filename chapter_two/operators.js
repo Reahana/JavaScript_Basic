3a9a81fa5d26e19015c226edcfed0b21f6d6d6a7
Amanda-G10 <reahana233@gmail.com>
2021-03-23 20:04:55 +0600

    operators

---
+++ chapter_two/operators.js
@@ -0,0 +1,76 @@
+// ****** Arithmetic Operator ******
+// + - * / % ++ --
+
+// var a =10
+// var b = 3
+
+// console.log(a%b);
+
+//Incremental
+//Pre- Incremental    Post-Incremental
+
+// console.log(++a);
+// console.log(a++);
+// console.log(a );
+
+// console.log(--b);
+
+// ****** Assignment Operator ******
+
+// var a =10
+// var b= 20
+
+// a = a + b
+// a += b
+// console.log(a)
+
+// a-=b
+// console.log(a);
+
+// a*=b
+// console.log(a);
+// a/=b
+// console.log(a);
+// a%=b
+// console.log(a);
+
+// ****** Comparism Operator ******
+// always returns boolean value true/false
+// var a = 10
+// var b = 20
+
+//  console.log(a ==b) // false
+// console.log(a !==b); //true
+
+// console.log(a>b); // false
+// console.log(a<b); //true
+
+// a=20
+
+// console.log(a>=b); //true
+// console.log(a<=b); //true
+
+// var c = '50'
+// var d = 50
+
+// console.log(c==d); //true
+// console.log(c===d); // false
+// console.log(c!==d); //true
+
+// ****** Logical Operator ******
+//&&  // AND logical
+//||  // OR logical
+//!  // NOT logical
+
+// ******  Bitwise Operator ******
+//&  //And binary number 
+//|  //OR binary number
+//~  // NOT  binary number
+//^  // EXOR
+//<<  //left shift
+//>>  //right shift
+
+
+// ****** typeof Operator ******
+// console.log(typeof(20));
+// console.log(typeof 'String');
