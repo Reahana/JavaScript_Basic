d1c1d6dfc1c0cf588c86914e2beb3cb7f4b5a410
Amanda-G10 <reahana233@gmail.com>
2021-03-24 09:53:58 +0600

    typeconversion

---
+++ chapter_two/typeConversion.js
@@ -0,0 +1,41 @@
+var str ='10000'
+var n =10
+
+
+console.log(str);
+console.log(n);
+console.log(Number.parseInt(str));
+console.log(n.toString());
+
+console.log(Infinity);
+
+console.log(Boolean(Infinity));
+console.log(Boolean(-Infinity));
+
+
+//Falsy Value
+''
+0
+null
+undefined
+NaN
+
+console.log(Boolean(''));
+console.log(Boolean('wegtwefe'));
+
+console.log(Boolean(null));
+console.log(Boolean(undefined));
+console.log(Boolean(NaN));
+
+console.log(Boolean(0));
+console.log(Boolean(475));
+
+
+console.log(true);
+var x = true
+
+console.log(x.toString());
+
+
+// ****Constructors
+//String Number Boolean
