935f4158ec74f48885d5e9886f3370764959aa31
Amanda-G10 <reahana233@gmail.com>
2021-03-25 11:42:20 +0600

    varriable

--- chapter_two/varriable.js
+++ chapter_two/varriable.js
@@ -2,3 +2,10 @@ var name = 'Amanda Reahana Gomes'
 var age = 23
 console.log(name + ' loves JavaScript');
 console.log(name + ' is ' + age + ' years old');
+
+
+//singel line comments
+
+/*Multi line
+comments
+never compile by compiler */
