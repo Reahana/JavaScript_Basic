dc9569a9704715da5cd97ca5be1d404067e520f8
Amanda-G10 <reahana233@gmail.com>
2021-04-05 11:13:44 +0600

    while loop

--- chapter_four/loops.js
+++ chapter_four/loops.js
@@ -57,8 +57,19 @@ until The condition becomes False.
 
  
 
- var i =0
- while (i<10) {
-     console.log('Reahana');
-    i++
- }
+//  var i =0
+//  while (i<10) {
+//      console.log('Reahana');
+//     i++
+//  }
+
+var isRunning = true
+while (isRunning) {
+    var rand = Math.floor(Math.random() * 10 +1 )
+    if (rand == 9) {
+        console.log('winner')
+        isRunning = false
+    } else {
+        console.log('Got ' + rand )
+    }
+}
