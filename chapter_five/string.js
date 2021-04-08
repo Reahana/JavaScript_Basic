5d8bc0a66fd3386fe8a3857d6b7cffc655fc4509
Amanda-G10 <reahana233@gmail.com>
2021-04-09 00:50:02 +0600

    string

---
+++ chapter_five/string.js
@@ -0,0 +1,16 @@
+//what is string
+
+// 'string' 
+// "string"
+
+var str = 'Something' // String Literal
+var str2 = String('Something') //String Constructor
+
+console.log(str2);
+
+
+var n =10
+//var str = n + '' // print as string
+// var str = n.toString()
+var str = String(n)
+console.log(str);
