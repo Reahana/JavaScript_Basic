af268edb13ed4cfb954af15f756ff5549e691ff5
Amanda-G10 <reahana233@gmail.com>
2021-04-20 00:19:39 +0600

    String

--- chapter_five/string.js
+++ chapter_five/string.js
@@ -3,14 +3,19 @@
 // 'string' 
 // "string"
 
-var str = 'Something' // String Literal
-var str2 = String('Something') //String Constructor
+// var str = 'Something' // String Literal
+// var str2 = String('Something') //String Constructor
 
-console.log(str2);
+// console.log(str2);
 
 
-var n =10
+// var n =10
 //var str = n + '' // print as string
 // var str = n.toString()
-var str = String(n)
-console.log(str);
+// var str = String(n)
+// console.log(str);
+
+
+//******Escape Notation */
+ var str = 'This is a String'
+ console.log(str);
