cfb5541f334f993911c62873d744a95fc5a7991e
Amanda-G10 <reahana233@gmail.com>
2021-06-23 20:22:55 +0600

    escape notation

--- chapter_five/string.js
+++ chapter_five/string.js
@@ -17,14 +17,14 @@
 
 
 //******Escape Notation */
- var str1 = 'This is a \'String\''
- console.log(str1);
- var str2 = 'This is a \nString'
- console.log(str2);
- var str3 = 'This is a \tString'
- console.log(str3);
- var str4 = 'This is a \\String'
- console.log(str4);
+//  var str1 = 'This is a \'String\''
+//  console.log(str1);
+//  var str2 = 'This is a \nString'
+//  console.log(str2);
+//  var str3 = 'This is a \tString'
+//  console.log(str3);
+//  var str4 = 'This is a \\String'
+//  console.log(str4);
 
  //carriage return \r
  //vertical tab \b
