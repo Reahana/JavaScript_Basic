4036aaebaa8e9db91637ffa016ec313f3dcab10b
Amanda-G10 <reahana233@gmail.com>
2021-07-19 19:14:51 +0600

    string, Literal vs Constructor

--- chapter_five/string.js
+++ chapter_five/string.js
@@ -16,17 +16,8 @@
 // console.log(str);
 
 
-//******Escape Notation */
-//  var str1 = 'This is a \'String\''
-//  console.log(str1);
-//  var str2 = 'This is a \nString'
-//  console.log(str2);
-//  var str3 = 'This is a \tString'
-//  console.log(str3);
-//  var str4 = 'This is a \\String'
-//  console.log(str4);
 
- //carriage return \r
- //vertical tab \b
- //backspace  \v
- //from feed  \f
+ 
+
+ 
+ 
